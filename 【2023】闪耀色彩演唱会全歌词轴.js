e5ffcca49246a91de6468ab91270186656ec80bb
helloakira <45556718+helloakira@users.noreply.github.com>
2023-04-18 10:37:30 +0800

    Add files via upload

---
+++ 【2023】闪耀色彩演唱会全歌词轴.js
@@ -0,0 +1,320 @@
+var SCLIVEAllArray = [
+	{
+		"SC_5thLIVE_DAY1": [
+			{
+				"songName": "【放クラ 5A】一閃は君が導く",
+				"startTime": "0:05:57.78",
+				"content": ""
+			},
+			{
+				"songName": "【アルスト 5B】Give me some more…",
+				"startTime": "0:09:24.74",
+				"content": ""
+			},
+			{
+				"songName": "【ストライ 4B】Overdrive Emotion",
+				"startTime": "0:13:24.16",
+				"content": ""
+			},
+			{
+				"songName": "【ノクチル 3A】Catch the Breeze",
+				"startTime": "0:17:13.52",
+				"content": ""
+			},
+			{
+				"songName": "【アンティーカ 5A】浮動性イノセンス",
+				"startTime": "0:21:25.34",
+				"content": ""
+			},
+			{
+				"songName": "【シーズ 2B】Bouncy Girl",
+				"startTime": "0:25:07.32",
+				"content": ""
+			},
+			{
+				"songName": "【イルミネ 5B】イルミネイトコンサート",
+				"startTime": "0:28:34.90",
+				"content": ""
+			},
+			{
+				"songName": "【アンティーカ 1A】バベルシティ・グレイス",
+				"startTime": "0:39:34.66",
+				"content": ""
+			},
+			{
+				"songName": "【ストライ 2A】Hide & Attack",
+				"startTime": "0:45:35.68",
+				"content": ""
+			},
+			{
+				"songName": "【放クラ 5B】キャットスクワッド",
+				"startTime": "0:50:01.60",
+				"content": ""
+			},
+			{
+				"songName": "【シーズ 2A】Fashionable",
+				"startTime": "0:54:19.08",
+				"content": ""
+			},
+			{
+				"songName": "【ノクチル 1A】いつだって僕らは",
+				"startTime": "0:58:41.72",
+				"content": ""
+			},
+			{
+				"songName": "【アルスト 3B】Anniversary",
+				"startTime": "1:07:18.56",
+				"content": ""
+			},
+			{
+				"songName": "【放クラ 4B】拝啓タイムカプセル",
+				"startTime": "1:13:41.20",
+				"content": ""
+			},
+			{
+				"songName": "【イルミネ 5A】FELICE",
+				"startTime": "1:18:21.30",
+				"content": ""
+			},
+			{
+				"songName": "【アンティーカ 3A】Black Reverie",
+				"startTime": "1:24:12.12",
+				"content": ""
+			},
+			{
+				"songName": "【イルミネ 4A】PRISISM",
+				"startTime": "1:28:42.42",
+				"content": ""
+			},
+			{
+				"songName": "【ストライ 4A】Tracing Defender",
+				"startTime": "1:33:24.88",
+				"content": ""
+			},
+			{
+				"songName": "【シーズ 1B】Fly and Fly",
+				"startTime": "1:36:49.14",
+				"content": ""
+			},
+			{
+				"songName": "【アルスト 5A】VERY BERRY LOVE",
+				"startTime": "1:41:22.28",
+				"content": ""
+			},
+			{
+				"songName": "【ノクチル 2B】今しかない瞬間を",
+				"startTime": "1:46:32.76",
+				"content": ""
+			},
+			{
+				"songName": "【ノクチル 3B】アスファルトを鳴らして",
+				"startTime": "2:00:50.01",
+				"content": ""
+			},
+			{
+				"songName": "【放クラ 1A】夢咲きAfter school",
+				"startTime": "2:05:25.08",
+				"content": ""
+			},
+			{
+				"songName": "【アンティーカ 5B】愚者の独白",
+				"startTime": "2:09:35.76",
+				"content": ""
+			},
+			{
+				"songName": "【シーズ 1A】OH MY GOD",
+				"startTime": "2:14:37.90",
+				"content": ""
+			},
+			{
+				"songName": "【ストライ 1A】Wandering Dream Chaser",
+				"startTime": "2:18:45.32",
+				"content": ""
+			},
+			{
+				"songName": "【アルスト 3A】ダブル・イフェクト",
+				"startTime": "2:23:07.04",
+				"content": ""
+			},
+			{
+				"songName": "【イルミネ 3A】Twinkle way",
+				"startTime": "2:27:58.16",
+				"content": ""
+			},
+			{
+				"songName": "【283PRO 4A】Resonance+",
+				"startTime": "2:33:49.54",
+				"content": ""
+			},
+			{
+				"songName": "【283PRO 2B】いつか Shiny Days",
+				"startTime": "2:47:37.44",
+				"content": ""
+			},
+			{
+				"songName": "【283PRO 1B】Multicolored Sky",
+				"startTime": "3:10:00.84",
+				"content": ""
+			},
+		]
+	},
+	{
+		"SC_5thLIVE_DAY2": [
+			{
+				"songName": "【放クラ 5A】一閃は君が導く",
+				"startTime": "0:06:16.44",
+				"content": ""
+			},
+			{
+				"songName": "【アルスト 5B】Give me some more…",
+				"startTime": "0:09:43.36",
+				"content": ""
+			},
+			{
+				"songName": "【ストライ 4B】Overdrive Emotion",
+				"startTime": "0:13:43.86",
+				"content": ""
+			},
+			{
+				"songName": "【ノクチル 3A】Catch the Breeze",
+				"startTime": "0:17:32.18",
+				"content": ""
+			},
+			{
+				"songName": "【アンティーカ 5A】浮動性イノセンス",
+				"startTime": "0:21:45.20",
+				"content": ""
+			},
+			{
+				"songName": "【シーズ 2B】Bouncy Girl",
+				"startTime": "0:25:26.98",
+				"content": ""
+			},
+			{
+				"songName": "【イルミネ 1A】ヒカリのdestination",
+				"startTime": "0:32:08.18",
+				"content": ""
+			},
+			{
+				"songName": "【放クラ 2A】ビーチブレイバー",
+				"startTime": "0:46:25.88",
+				"content": ""
+			},
+			{
+				"songName": "【アンティーカ 2A】NEO THEORY FANTASY",
+				"startTime": "0:52:01.64",
+				"content": ""
+			},
+			{
+				"songName": "【シーズ 1B】Fly and Fly",
+				"startTime": "0:56:57.78",
+				"content": ""
+			},
+			{
+				"songName": "【ストライ 1B】Transcending The World",
+				"startTime": "1:01:15.20",
+				"content": ""
+			},
+			{
+				"songName": "【ノクチル 2A】僕らだけの未来の空",
+				"startTime": "1:05:51.06",
+				"content": ""
+			},
+			{
+				"songName": "【イルミネ 5A】FELICE",
+				"startTime": "1:12:38.86",
+				"content": ""
+			},
+			{
+				"songName": "【アンティーカ 5B】愚者の独白",
+				"startTime": "1:16:54.26",
+				"content": ""
+			},
+			{
+				"songName": "【アルスト 5A】VERY BERRY LOVE",
+				"startTime": "1:21:43.22",
+				"content": ""
+			},
+			{
+				"songName": "【ノクチル 1B】あの花のように",
+				"startTime": "1:25:37.16",
+				"content": ""
+			},
+			{
+				"songName": "【アルスト 2B】Love Addiction",
+				"startTime": "1:30:46.32",
+				"content": ""
+			},
+			{
+				"songName": "【ストライ 3A】Another Rampage",
+				"startTime": "1:35:18.74",
+				"content": ""
+			},
+			{
+				"songName": "【シーズ 1A】OH MY GOD",
+				"startTime": "1:39:04.80",
+				"content": ""
+			},
+			{
+				"songName": "【放クラ 5B】キャットスクワッド",
+				"startTime": "1:43:14.76",
+				"content": ""
+			},
+			{
+				"songName": "【シーズ 2A】Fashionable",
+				"startTime": "1:53:39.92",
+				"content": ""
+			},
+			{
+				"songName": "【イルミネ 5B】イルミネイトコンサート",
+				"startTime": "1:58:00.04",
+				"content": ""
+			},
+			{
+				"songName": "【放クラ 1B】太陽キッス",
+				"startTime": "2:01:24.16",
+				"content": ""
+			},
+			{
+				"songName": "【ストライ 4A】Tracing Defender",
+				"startTime": "2:06:55.74",
+				"content": ""
+			},
+			{
+				"songName": "【アルスト 1A】アルストロメリア",
+				"startTime": "2:10:44.52",
+				"content": ""
+			},
+			{
+				"songName": "【ノクチル 3B】アスファルトを鳴らして",
+				"startTime": "2:16:17.60",
+				"content": ""
+			},
+			{
+				"songName": "【アンティーカ 3B】純白トロイメライ",
+				"startTime": "2:21:04.10",
+				"content": ""
+			},
+			{
+				"songName": "【イルミネ 4B】スマイルシンフォニア",
+				"startTime": "2:25:50.80",
+				"content": ""
+			},
+			{
+				"songName": "【283PRO 1A】Spread the Wings!!",
+				"startTime": "2:31:33.00",
+				"content": ""
+			},
+			{
+				"songName": "【283PRO 3B】Dye the sky",
+				"startTime": "2:50:43.64",
+				"content": ""
+			},
+			{
+				"songName": "【283PRO 1B】Multicolored Sky",
+				"startTime": "3:21:37.04",
+				"content": ""
+			},
+		]
+	}
+]
